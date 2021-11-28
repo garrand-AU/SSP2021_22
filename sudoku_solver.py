9c16bcb1624dfa593c9e8cd5f6cfb98d8cc4b14c
YK Chen <54578997+garrand-AU@users.noreply.github.com>
2021-11-28 11:09:40 +1030

    Create sudoku_solver.py
    Use recursion method to brute force the result.
    Time Complexity: O(9! * 8! * 7! * 6! * 5! * 4! * 3! * 2! * 1!) = O((9!)^9) = O(1)
    Space Complexity: O(81) = O(1) (recursion depth)

---
+++ sudoku_solver.py
@@ -0,0 +1,39 @@
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        rows, cols, boxes = defaultdict(set), defaultdict(set), defaultdict(set)
+        q = []
+        vals = [str(n) for n in range(1, 10)]
+
+        for r in range(9):
+            for c in range(9):
+                if (v := board[r][c]) == '.':
+                    q.append((r, c))
+                else:
+                    rows[r].add(v)
+                    cols[c].add(v)
+                    boxes[(r//3, c//3)].add(v)
+
+        def dfs():
+            if not q:
+                return True
+            r, c = q.pop()
+            b = (r//3, c//3)
+            for v in vals:
+                if v not in rows[r] and v not in cols[c] and v not in boxes[b]:
+                    board[r][c] = v
+                    rows[r].add(v)
+                    cols[c].add(v)
+                    boxes[b].add(v)
+                    if dfs():
+                        return True
+                    board[r][c] = '.'
+                    rows[r].discard(v)
+                    cols[c].discard(v)
+                    boxes[b].discard(v)
+            q.append((r, c))
+            return False
+
+        dfs()
