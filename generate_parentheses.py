83ff6bc9d982bf2a1bea8e75846c73a4cec5267a
YK Chen <54578997+garrand-AU@users.noreply.github.com>
2021-11-28 11:23:08 +1030

    Update generate_parentheses.py
    Add the complexity analysis

--- generate_parentheses.py
+++ generate_parentheses.py
@@ -7,7 +7,8 @@ class Solution:
 
         # put the paremthese at the right of previous parethese
 
-
+        # This is the n-th Catalan number, where the first Catalan numbers for n = 0, 1, 2, 3, ... are 1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862, 16796, 58786,...
+        # Time & Space: n-th Catalan Number.
 
         def generate(p, left, right, results=[]):
             if left:         generate(p + '(', left-1, right)
